20d927f788dfa0eb101e9b10858a6f2a4f3c0c97
arash <vahaabpour@gmail.com>
2021-01-27 21:01:43 -0800

    minor bug fix

--- a2c_ppo_acktr/algo/ppo.py
+++ a2c_ppo_acktr/algo/ppo.py
@@ -86,7 +86,7 @@ class PPO:
                 if self.sog_gail:
                     expert_state, expert_action = next(sog_train_loader)
                     expert_state = obsfilt(expert_state.numpy(), update=False)
-                    expert_state = torch.tensor(expert_state, dtype=torch.float32, device=self.device)[None]
+                    expert_state = torch.tensor(expert_state, dtype=torch.float32, device=self.device)
                     expert_action = expert_action.to(self.device)
                     sog_loss = self.sog.predict_loss(expert_state, expert_action)
                     loss += sog_loss * self.sog_gail_coef
